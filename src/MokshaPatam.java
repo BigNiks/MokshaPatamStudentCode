85cebaa82cbf0fd768d146e982f8859e6b4d6b28
BigNiks <niko.madriz@menloschool.org>
2024-09-09 00:11:07 -0700

    final

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Moksha Patam
  * A puzzle created by Zach Blick
@@ -9,37 +13,53 @@
  */
 
 public class MokshaPatam {
-
+    private static Queue<Integer> queue = new LinkedList<>();
+    private static int currentNode;
+    private static int[] checkLad;
+    private static int[] checkSnake;
+    private static int[] numRolls;
+    private static int node;
     /**
      * TODO: Complete this function, fewestMoves(), to return the minimum number of moves
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
+        checkLad = new int[boardsize + 1];
+        checkSnake = new int[boardsize + 1];
+        numRolls = new int[boardsize + 1];
+        for (int i = 0; i < ladders.length; i++) {
+            checkLad[ladders[i][0]] = ladders[i][1];
+        }
 
-        return 0;
-    }
+        for (int i = 0; i < snakes.length; i++) {
+            checkSnake[snakes[i][0]] = snakes[i][1];
+        }
 
-    public static int largeLad(int[][] ladders) {
-        int biggest = ladders[0][0];
-        for (int i = 0; i < ladders[0].length; i++) {
-            for (int j = 0; j < ladders.length; j++) {
-                if (ladders[i][j] > biggest) {
-                    biggest = ladders[i][j];
+        queue.add(1);
+
+        while (!queue.isEmpty()) {
+            currentNode = (int) queue.remove();
+            if (currentNode == boardsize) {
+                return numRolls[currentNode];
+            }
+            for (int k = 1; k < 7; k++) {
+                node = currentNode + k;
+                if (node <= boardsize) {
+                    if (checkLad[node] != 0) {
+                        node = checkLad[node];
+                    }
+                    if (checkSnake[node] != 0) {
+                        node = checkSnake[node];
+                    }
+                    if (numRolls[node] == 0) {
+                        numRolls[node] += numRolls[currentNode] + 1;
+                        queue.add(node);
+                    }
                 }
             }
         }
-        return biggest;
+        return -1;
     }
 
-    public static int largeSnake(int[][] snakes) {
-        int biggest = snakes[0][0];
-        for (int i = 0; i < snakes[0].length; i++) {
-            for (int j = 0; j < snakes.length; j++) {
-                if (snakes[i][j] > biggest) {
-                    biggest = snakes[i][j];
-                }
-            }
-        }
-        return biggest;
-    }
+
 }
